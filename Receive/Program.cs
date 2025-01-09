88e92d4075f5be78fd235638ca6bfe53c3d9e07c
diegoNZ04 <diegoamorim03152004@gmail.com>
2025-01-09 01:15:54 -0300

    'initial'

---
+++ Receive/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync
+(
+    queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+// Solicita a entrega das mensagens e fornece retorno de chamada
+var consumer = new AsyncEventingBasicConsumer(channel);
+// Recebe a mensagem da fila
+consumer.ReceivedAsync += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    // Converte para string
+    var message = Encoding.UTF8.GetString(body);
+    // Imprime a mensagem
+    Console.WriteLine($" [x] Received {message}");
+    return Task.CompletedTask;
+};
+
+// Indica o consumo da mensagem
+await channel.BasicConsumeAsync
+(
+    "hello",
+    autoAck: true,
+    consumer: consumer
+);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ Send/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+// Conexão ao nó RabbitMQ
+var factory = new ConnectionFactory { HostName = "localhost" };
+// Abre conexão com o nó
+using var connection = await factory.CreateConnectionAsync();
+// Cria canal onde a fila será definida
+using var channel = await connection.CreateChannelAsync();
+
+// Cria fila 
+await channel.QueueDeclareAsync
+(
+    queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+// Cria mensagem a ser posta na fila
+const string message = "Hello World!";
+// Codifica a mensagem em um array de bytes
+var body = Encoding.UTF8.GetBytes(message);
+
+// Publica a mesagem informando a fila e o corpo da mensagem
+await channel.BasicPublishAsync
+(
+    exchange: string.Empty,
+    routingKey: "hello",
+    body: body
+);
+
+Console.WriteLine($" [X] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
