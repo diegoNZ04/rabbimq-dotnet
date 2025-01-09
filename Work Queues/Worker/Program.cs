c6f075d50d0ce38bf95f03076ba9859720bbb7ac
diegoNZ04 <diegoamorim03152004@gmail.com>
2025-01-09 19:27:06 -0300

    Work Queues: Round-robin Dispatching

---
+++ Work Queues/Worker/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync
+(
+    queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    byte[] body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+
+    int dots = message.Split('.').Length - 1;
+    await Task.Delay(dots * 1000);
+
+    Console.WriteLine(" [x] Done");
+};
+
+await channel.BasicConsumeAsync
+(
+    "hello",
+    autoAck: true,
+    consumer: consumer
+);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
